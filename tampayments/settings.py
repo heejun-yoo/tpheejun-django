3ec17f51a33f7f46e9e2cf2d8e5ba1a7363d7e11
tpheejun <heejunyoo@tosspayments.com>
2022-10-11 14:26:43 +0900

    update settings

--- tampayments/settings.py
+++ tampayments/settings.py
@@ -87,10 +87,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "tampayments",
-        "USER": "name",
-        "PASSWORD": "",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": 5432,
     }
 }
 
--- tampayments/wsgi.py
+++ tampayments/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tampayments.settings')
 
