9b30d3d1c83b1b5eb9a935cfa7506af2c7b0d90b
tpheejun <heejunyoo@tosspayments.com>
2022-10-11 14:47:27 +0900

    update settings

--- tampayments/settings.py
+++ tampayments/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-#import django_heroku
+import django_heroku
 from pathlib import Path
 import os
 import dj_database_url
@@ -139,4 +139,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
